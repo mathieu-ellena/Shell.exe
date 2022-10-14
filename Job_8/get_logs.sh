04315434fef375dc2ab884201b0d3039b105b303
mathieu-ellena <mathieu.ellena@laplateforme.io>
2022-10-14 15:00:15 +0200

    add Job 8

--- Job_8/get_logs.sh
+++ Job_8/get_logs.sh
@@ -1,3 +1,7 @@
-echo 'date' >> date-out.txt
+Date=$(date +%d-%m-%y-%H:%M)
+file=number_connection-$Date
 
-
+last kaiden | wc -l >> $file
+tar -czvf /home/kaiden/Github/Shell.exe/Job_8/$file.tar.gz $file
+mv $file.tar.gz Backup
+rm $file
